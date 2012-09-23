142ecf12608e5809d6b82155417af01944d73eb1
Laurence Emms <laurence@laurence-MS-7345.(none)>
2012-09-23 00:05:03 -0700

    Added freeglut support

--- demo/main.cpp
+++ demo/main.cpp
@@ -4,14 +4,15 @@ Siggraph Asia 2012 Demo
 Laurence Emms
 */
 
-
 #include <iostream>
 using namespace std;
 
+// OpenGL
 #include <GL/glew.h>
 #include <GL/glut.h>
-// TODO: build to FreeGLUT
-//#include <GL/freeglut_ext.h>
+#ifdef FREEGLUT
+	#include <GL/freeglut_ext.h>
+#endif
 
 int main(int argc, char **argv)
 {
@@ -19,7 +20,22 @@ int main(int argc, char **argv)
 	}
 	cout << "Siggraph Asia 2012 Demo" << endl;
 
+	// setup glut
 	glutInit(&argc, argv);
+#ifdef FREEGLUT
+	cout << "FreeGLUT" << endl;
+	if (glutGet(GLUT_VERSION) < 20001) {
+	    cout << "Sorry, you need freeglut version 2.0.1 or later to run this program." << endl;
+	    return 1;
+	}
+#else
+	cout << "GLUT" << endl;
+#endif
+	glutInitDisplayMode(GLUT_DEPTH | 
+			GLUT_DOUBLE | 
+			GLUT_RGBA | 
+			GLUT_MULTISAMPLE);
+	glutInitWindowPosition(0, 0);
 
 	return 0;
 }
