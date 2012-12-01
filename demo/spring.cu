a9541ce477a773ef7b0853964b07b5dd021bc68c
Laurence Emms <laurenceemms@gmail.com>
2012-12-01 12:49:38 +0800

    Added copyright notice.

--- demo/spring.cu
+++ demo/spring.cu
@@ -3,7 +3,23 @@ Siggraph Asia 2012 Demo
 
 Spring vector implementation.
 
-Laurence Emms
+This file is part of SigAsiaDemo2012.
+
+SigAsiaDemo2012 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SigAsiaDemo2012 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SigAsiaDemo2012.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2012 Laurence Emms
+
 */
 
 #include <iostream>
